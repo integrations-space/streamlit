e0e00e7219a1d90164c329df5af0bbfe2a248cc3
youngpong <youngpong@gmail.com>
2023-01-20 01:38:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Uber pickups in NYC')
-st.write("tinkter - share program - share without relying on web fundamental (Hardcore skillset))
+st.write("tinkter - share program - share without relying on web fundamental Hardcore skillset")
 st.write("Plotly - webbased") 
 st.write("Streamlit - quick start for programming")
 st.write("thunkeble - mobile for fun")
