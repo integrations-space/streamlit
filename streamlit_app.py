a5c354e3673c694fca7182a4794cffed723de04b
youngpong <youngpong@gmail.com>
2023-01-20 01:30:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Uber pickups in NYC')
+streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/streamlit_app.py
