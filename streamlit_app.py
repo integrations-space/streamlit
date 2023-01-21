c376886d8f63e45c14db387bd9beedd4a9e1f129
youngpong <youngpong@gmail.com>
2023-01-21 09:52:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Programming with GitHub')
-st.write("tinkter - share program - share without relying on web, fundamental, Hardcore skillset")
-st.write("Plotly - webbased") 
-st.write("Streamlit - quick start for programming")
-st.write("thunkeble - mobile for fun")
+st.text('''tinkter - share program - share without relying on web, fundamental, Hardcore skillset
+           Plotly - webbased
+           Streamlit - quick start for programming
+           thunkeble - mobile for fun''')
