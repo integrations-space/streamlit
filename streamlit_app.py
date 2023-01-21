667ca63ca78ea35c657c3151c414eac0ebcc7627
youngpong <youngpong@gmail.com>
2023-01-21 09:53:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 st.title('Programming with GitHub')
 st.text('''tinkter - share program - share without relying on web, fundamental, Hardcore skillset
-           Plotly - webbased
-           Streamlit - quick start for programming
-           thunkeble - mobile for fun''')
+Plotly - webbased
+Streamlit - quick start for programming
+thunkeble - mobile for fun''')
