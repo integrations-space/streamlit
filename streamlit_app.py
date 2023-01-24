f4446e2e2202a6179c0f3ccfb22e8c3c01de04fe
youngpong <youngpong@gmail.com>
2023-01-24 21:47:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,30 @@ import numpy as np
 #import { IfcLoader } from "web-ifc-three";
 #import { Scene } from "three";
 st.title('Programming with GitHub')
-#multiline text
+
 st.text('''
 + tinkter - share program - share without relying on web, fundamental, Hardcore skillset
 + Plotly - webbased
 + Streamlit - quick start for programming
 + thunkeble - mobile for fun
+
+Downloadable healthcare models available online
+https://www.wbdg.org/ffc/dha/mhs-space-templates
+
 + Create a BIM application with python in under 60 minutes: https://youtu.be/AfQztEUSQns
+
 + access ifc with Python:http://blog.ifcopenshell.org/
-+ access group parameter: https://dynamopythonprimer.gitbook.io/dynamo-python-primer/4-revit-specific-topics/working-with-parameters/instance-parameters
+
 + getting started with Visual Studio Code,VS Code, a source-code editor made by Microsoft with the Electron Framework, for Windows, Linux and macOS 
 https://code.visualstudio.com/docs/python/python-tutorial
 + use of blender for ifc: https://blenderbim.org/docs-python/ifcconvert/installation.html
 + Guide for blender download: https://blenderbim.org/docs-python/ifcconvert/installation.html
-+ Guide for blender-python with ifcopenshell: https://blenderbim.org/docs-python/ifcopenshell/installation.html''')
++ Guide for blender-python with ifcopenshell: https://blenderbim.org/docs-python/ifcopenshell/installation.html
 
-
-#Creates THREE.js scene
-#const scene = new Scene();
-
-#...
-
-#Loads IFC and adds it to the scene
-#const ifcLoader = new IfcLoader();
-#ifcLoader.load(ifcURL, (geometry) => scene.add(geometry));
+st.title('Dynamo')
++ Pushing Shared parameters into family 
+https://www.youtube.com/watch?v=lvO2_0IQ8vQ
+https://www.youtube.com/watch?v=6Az1G0pla_k
++ access group parameter: https://dynamopythonprimer.gitbook.io/dynamo-python-primer/4-revit-specific-topics/working-with-parameters/instance-parameters
+Automated modeling by placing models in points set in the model
+https://www.youtube.com/watch?v=bvxeY60VGT8''')
