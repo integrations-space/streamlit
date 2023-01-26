85ff2e1d735765fa505366af55abed1d42342669
youngpong <youngpong@gmail.com>
2023-01-26 09:24:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,38 @@ https://www.youtube.com/watch?v=6Az1G0pla_k
 + access group parameter: https://dynamopythonprimer.gitbook.io/dynamo-python-primer/4-revit-specific-topics/working-with-parameters/instance-parameters
 Automated modeling by placing models in points set in the model
 https://www.youtube.com/watch?v=bvxeY60VGT8''')
+
+import ifcopenshell
+import os
+
+file_path = input("Please enter the file path for the IFC file: ")
+
+try:
+    ifc_file = open(file_path, "r")
+    print("File opened successfully!")
+except FileNotFoundError:
+    print("The file could not be found. Please check the file path and try again.")
+except:
+    print("An error occurred while trying to open the file.")
+    
+# Open the IFC file
+ifc_file = ifcopenshell.open(file_path)
+
+# Get the entities in the file
+entities = ifc_file.by_type("IfcProduct")
+
+# Define a recursive function to print the tree
+def print_tree(entity, level=0):
+    # Print the entity's name and type
+    print("    " * level + entity.is_a() + ": " + entity.Name)
+
+    # Check if the entity has children (contained objects)
+    if hasattr(entity, "IsDecomposedBy"):
+        # If it does, recursively call the function for each child
+        for rel in entity.IsDecomposedBy:
+            for child in rel.RelatedObjects:
+                print_tree(child, level + 1)
+
+# Call the function for each top-level entity in the file
+for e in entities:
+    print_tree(e)
