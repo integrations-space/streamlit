8232beed4b9996638d582cd2795af02073ac6e6b
Innovations & Integrations <youngpong@gmail.com>
2024-11-08 18:30:52 +0800

    Update methodology.py

--- pages/methodology.py
+++ pages/methodology.py
@@ -4,17 +4,25 @@ import streamlit as st
 def app():
     st.header("[ Methodology ]")
     st.write("""
-    This page provides a comprehensive overview of the data flows and implementation details used in the application.
-    The methodology covers data extraction, AI model utilization, and compliance checks through a structured flow.
+    This page provides a comprehensive overview of the data flows and implementation details used in the application. The methodology covers data extraction, AI model utilization, and compliance checks through a structured flow.
     
-    **Data Flows and Implementation Details**:
-    - The project is using Google Cloud Storage (GCS) for data storage and management.
-    - AI agents are assigned modular tasks such as data parsing, analysis, cleaning, exporting, and validating information extracted from input documents.
+    **Flowchart of Data, Workflow, and Implementation Details**:
     
-    **Flowchart of Data and Workflow**:
-    - Parsing Design Intent and Requirements (using the Google Gemini-1.5-Pro-002 model): Extract, parse, clean, and tabulate into structured data for easier understanding and use.
+    - Data Input (stored in GCS): A preliminary dataset, including window_schedule.jpg, approveddoc.pdf, and scdf_chapter_4.pdf, serves as the initial input.
+    
+    - Parsing Design Intent and Requirements (using the Google Gemini-1.5-Pro-002 model): Extract, parse, clean, and tabulate data into structured formats for easier understanding and usage.
+    
     - OpenAI-Powered Validation (using the OpenAI GPT-4o-mini model): Validate information accuracy by allowing users to upload data and request specific checks.
+    
+    Data Output (stored in GCS):
+    
+    - Agents 1 & 2 produce structured data in Excel format (window_schedule.xls).
+    - Agents 2 & 4 translate the PDF documents into text format, saved as Requirements.txt.
+    - Agent 5 generates a structured output named check01.xls based on window_schedule.xls and Requirements.txt.
+    - An independent UI, based on the OpenAI model, was developed to validate uploaded text files according to users' specific queries.
+    
     ![Flowchart Image](https://raw.githubusercontent.com/integrations-space/streamlit/main/pictures/WorkflowDiagram.jpg)
+    - 
     """)
 
 # Run the app if the script is executed directly
