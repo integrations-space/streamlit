4412a58b624f07181166db7d787421d74dbe9e35
Innovations & Integrations <youngpong@gmail.com>
2024-11-08 18:43:08 +0800

    Update methodology.py

--- pages/methodology.py
+++ pages/methodology.py
@@ -4,21 +4,21 @@ import streamlit as st
 def app():
     st.header("[ Methodology ]")
     st.write("""
-    This page provides a comprehensive overview of the data flows and implementation details used in the application. The methodology covers data extraction, AI model utilization, and compliance checks through a structured flow.
-    
-    Data Input (stored in GCS): 
+    This page provides a comprehensive overview of the implementation details and workflow for the proof of concept. The methodology covers data extraction from provided sources, utilizing different AI models for processing, analysis, and compliance checks through a modular and structured workflow.  
+
+    Data Input (stored in GCS):
     - A preliminary dataset, including window_schedule.jpg, approveddoc.pdf, and scdf_chapter_4.pdf, serves as the initial input.
-      
-    Data Output (stored in GCS) and Implementation details:
+    
+    Implementation Details and Outputs (stored in GCS):
     - Agents 1 & 2 produce structured data in Excel format (window_schedule.xls).
     - Agents 2 & 4 translate the PDF documents into text format, saved as Requirements.txt.
     - Agent 5 generates a structured output named check01.xls based on window_schedule.xls and Requirements.txt.
+    - An independent UI, based on the OpenAI model, was developed to validate uploaded text files according to users' specific queries.   
     
-    Workflow:
+    AI Models:
     - Parsing Design Intent and Requirements (using the Google Gemini-1.5-Pro-002 model): Extract, parse, clean, and tabulate data into structured formats for easier understanding and usage.
     - OpenAI-Powered Validation (using the OpenAI GPT-4o-mini model): Validate information accuracy by allowing users to upload data and request specific checks.
-    - An independent UI, based on the OpenAI model, was developed to validate uploaded text files according to users' specific queries.
-    
+
     ![Flowchart Image](https://raw.githubusercontent.com/integrations-space/streamlit/main/pictures/WorkflowDiagram.jpg)
     - 
     """)
