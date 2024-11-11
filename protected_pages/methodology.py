f9703060646d984b17618a894da9a291ef4dc9e3
Innovations & Integrations <youngpong@gmail.com>
2024-11-11 11:04:58 +0800

    Updated methodology image width with description update

--- protected_pages/methodology.py
+++ protected_pages/methodology.py
@@ -4,23 +4,27 @@ import streamlit as st
 def app():
     st.title("[ Methodology ]")
     st.write("""
-    This page provides a comprehensive overview of the proof-of-concept's implementation details and workflow, including data extraction from non-sensitive data on Google Cloud Storage (GCS), AI-powered processing, analysis, and compliance checks through modular applications.
-    Data Input:
+    This page provides a comprehensive overview of the proof-of-concept's dataset, implementation details, AI models used for different purposes and workflow diagram.
+    """)
+    st.subheader("Dataset:")
+    st.write("""
     - A preliminary dataset, consisting of window_schedule.jpg, approveddoc.pdf, and scdf_chapter_4.pdf, serves as the initial input for the applications.
-    
-    Implementation using AI agents, with outputs stored in GCS:
+    """)
+    st.subheader("Implementation details:")
+    st.write("""
     - Agent 1 and Agent 2 produce structured data in Excel format (window_schedule.xls).
     - Agent 2 and Agent 4 translate the PDF documents into text format, saved as Requirements.txt.
     - Agent 5 generates a structured output named check01.xls based on window_schedule.xls and Requirements.txt.
     - An independent UI, based on the OpenAI model, was developed to validate uploaded text files according to users' specific queries.   
-    
-    AI Models used for different purposes:
+    """)
+    st.subheader("AI Models used for different purposes:")
+    st.write("""
     - Parsing Design Intent and Requirements (using the Google Gemini-1.5-Pro-002 model): Extract, parse, clean, and tabulate data into structured formats for easier understanding and usage.
     - OpenAI-Powered Validation (using the OpenAI GPT-4o-mini model): Validate information accuracy by allowing users to upload data and request specific checks.
-
-    ![Flowchart Image](https://raw.githubusercontent.com/integrations-space/streamlit/main/pictures/WorkflowDiagram.jpg)
-    - 
     """)
+    st.subheader("Workflow Diagram")
+    # Add the image with a limited width
+    st.image("https://raw.githubusercontent.com/integrations-space/streamlit/main/pictures/WorkflowDiagram.jpg", width=700)
 
 # Run the app if the script is executed directly
 if __name__ == "__main__":
