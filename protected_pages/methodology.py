c90fecd79723c3f9b55a4a99062e6dbaf609e88c
Innovations & Integrations <youngpong@gmail.com>
2024-11-15 09:53:15 +0800

    Update methodology.py

--- protected_pages/methodology.py
+++ protected_pages/methodology.py
@@ -4,16 +4,13 @@ import streamlit as st
 def app():
     st.title("[ Methodology ]")
     st.write("""
-    This page provides a comprehensive overview of the proof-of-concept's dataset, implementation details, AI models used for different purposes and workflow diagram.
+    This page provides a comprehensive overview of the proof-of-concept's implementation details, AI models used for different purposes, dataset and workflow diagram.
     """)
-    st.subheader("Dataset:")
-    st.write("""
-    - A preliminary dataset, consisting of window_schedule.jpg, approveddoc.pdf, and scdf_chapter_4.pdf, serves as the initial input for the applications.
-    """)
+    
     st.subheader("Implementation details:")
     st.write("""
     - Agent 1 and Agent 2 produce structured data in Excel format (window_schedule.xls).
-    - Agent 2 and Agent 4 translate the PDF documents into text format, saved as Requirements.txt.
+    - Agent 3 and Agent 4 translate the PDF documents into text format, saved as Requirements.txt.
     - Agent 5 generates a structured output named check01.xls based on window_schedule.xls and Requirements.txt.
     - An independent UI, based on the OpenAI model, was developed to validate uploaded text files according to users' specific queries.   
     """)
@@ -21,6 +18,10 @@ def app():
     st.write("""
     - Parsing Design Intent and Requirements (using the Google Gemini-1.5-Pro-002 model): Extract, parse, clean, and tabulate data into structured formats for easier understanding and usage.
     - OpenAI-Powered Validation (using the OpenAI GPT-4o-mini model): Validate information accuracy by allowing users to upload data and request specific checks.
+    """)
+    st.subheader("Dataset:")
+    st.write("""
+    - A preliminary dataset, consisting of window_schedule.jpg, approveddoc.pdf, and scdf_chapter_4.pdf, serves as the initial input for the applications.
     """)
     st.subheader("Workflow Diagram")
     # Add the image with a limited width
