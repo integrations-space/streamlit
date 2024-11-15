9168ece3943ef773828c7ec6242123d109233ecc
Innovations & Integrations <youngpong@gmail.com>
2024-11-15 16:37:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,12 +97,13 @@ elif selected_page == "Methodology":
     load_protected_page("methodology")  # Load the methodology page
 elif selected_page == "Proposed Solution / PoC":
 # Create an expander for the notice on errors
-    with st.expander("⚠️  ### Notice  ⚠️"):
+    with st.expander("⚠️  Error Notice   ⚠️"):
         st.write("""
         - We have completed this Proof of Concept (PoC) project. 
         - Billing for Google services such as Google Cloud Storage (GCS) and Vertex AI has been deactivated to prevent further costs. 
         - As a result, the first three buttons will display two error messages when attempting to retrieve data from GCS. 
         - However, for reference and ease of understanding, we have included previously generated outputs for demonstration.
+        - We are exploring available FoC AI models and will update the code once ready.
         - Thank you for your understanding.
         """)
     # Set up Google Cloud credentials using secrets
